549bdead28dc45cb0b86a74e98749505dd6a4939
istadius.pproyectos istadius.pproyectos <study.oso@gmail.com>
2019-04-27 02:27:34 -0300

    Punto 2 (Invarianzas) funcionando. Punto 1 (Contratos) ahora soporta herencia y con un poco de maña puede incluir contratos desde módulos. Todavia hay que emprolijar el código y reemplazar alguna cosa fea

--- ruby/lib/contracts.rb
+++ ruby/lib/contracts.rb
@@ -2,22 +2,69 @@
 module Contracts
   
   #El módulo ClassMethods incluye en si los métodos de clase que van a agregarse a la clase objetivo
-  module ClassMethods
+  module ContractsClassMethods
     
-    #Estas dos listas van a almacenar los procs/bloques que voy a querer ejecutar en los contratos
-    @@__beforeBlocks = []  
-    @@__afterBlocks = []
+    #Estos dos diccionarios van a almacenar los procs que voy a querer ejecutar en los contratos
+    #Son diccionarios que adentro tienen arrays/listas/como se llamen en ruby
+    @@__beforeBlocks = Hash.new
+    @@__afterBlocks = Hash.new
     
     #Esta variable la voy a utilizar mas abajo para evitar que mi código caiga en llamadas recursivas infinitas
     @@__lastMethodAdded = nil
     
+    #Inicializa listas dentro de los diccionarios para esta clase/módulo
+    def initLists
+      if(!@@__beforeBlocks[self.name])
+        @@__beforeBlocks[self.name] = []
+        @@__afterBlocks[self.name] = []
+      end
+    end
+    
     #Este método es el que voy a usar para definir los contratos en las clases que incluyan el modulo Contracts
-    #Es un método de clase y recibe como parámetros dos procs/bloques
+    #Es un método de clase y recibe como parámetros dos procs
     def before_and_after_each_call(beforeBlock, afterBlock)
-      
+      initLists()
       #Simplemente agrega los bloques recibidos a las listas correspondientes
-      @@__beforeBlocks.push(beforeBlock)
-      @@__afterBlocks.push(afterBlock)
+      @@__beforeBlocks[self.name].push(beforeBlock)
+      @@__afterBlocks[self.name].push(afterBlock)
+    end
+    
+    #Método que utilizo para heredar contratos de ancestros
+    def inheritContracts
+      initLists()
+      
+      myClass = self
+      
+      #Todo: cambiar este pseudo try catch por algo menos feo
+      begin
+        while (myClass.name != 'Class' && myClass.superclass)
+
+          myClass = myClass.superclass
+          if(@@__beforeBlocks[myClass.name])
+            @@__beforeBlocks[myClass.name].each { |blck| @@__beforeBlocks[self.name].push(blck) }
+          end
+
+          if(@@__afterBlocks[myClass.name])
+            @@__afterBlocks[myClass.name].each { |blck| @@__afterBlocks[self.name].push(blck) }
+          end
+        end
+      rescue
+        
+      end
+    end
+    
+    #Método que utilizo para incluir contratos de módulos
+    def includeContracts
+      initLists()
+      self.included_modules.each {|modl| 
+        if(@@__beforeBlocks[modl.name])
+          @@__beforeBlocks[modl.name].each { |blck| @@__beforeBlocks[self.name].push(blck) }
+        end
+
+        if(@@__afterBlocks[modl.name])
+          @@__afterBlocks[modl.name].each { |blck| @@__afterBlocks[self.name].push(blck) }
+        end
+      }
     end
     
     #Esto es un hook de ruby que se llama cada vez que un nuevo método es definido (en una clase o un módulo)
@@ -37,9 +84,13 @@ module Contracts
         
         #Defino un nuevo método que va a ser llamado en lugar del método original
         define_method custom do |*args, &block|
-          @@__beforeBlocks.each { |blck| blck.call } #Ejecuto todos los bloques before que tenia almacenados
+          if(@@__beforeBlocks[self.class.name])
+            @@__beforeBlocks[self.class.name].each { |blck| blck.call(self) } #Ejecuto todos los bloques before que tenia almacenados
+          end
           send original, *args, &block #Ejecuto el método original por su nombre alternativo
-          @@__afterBlocks.each { |blck| blck.call } #Ejecuto todos los bloques after que tenia almacenados
+          if(@@__afterBlocks[self.class.name])
+            @@__afterBlocks[self.class.name].each { |blck| blck.call(self) } #Ejecuto todos los bloques after que tenia almacenados
+          end
         end
         
         #Defino los alias usando los identificadores previamente creados
@@ -51,13 +102,15 @@ module Contracts
   
   #Cuando el módulo es incluido
   def self.included(base)
-    base.extend(ClassMethods) #Agrego los métodos del módulo ClassMethods como métodos de clase al modulo/clase que me está incluyendo
+    base.extend(ContractsClassMethods) #Agrego los métodos del módulo ClassMethods como métodos de clase al modulo/clase que me está incluyendo
+    base.inheritContracts
+    #base.includeContracts Esto por ahora acá no hace nada, hay que llamarlo al terminar de incluir los módulos
   end
 end
 
 #Módulos y clases de ejemplo para testear los contratos
 module TestModule
-  include Contracts;
+  include Contracts
   
   before_and_after_each_call(proc{ puts 'Before from module' },  proc{ puts 'After from module' })
   
@@ -67,7 +120,7 @@ module TestModule
 end
 
 class ParentTestClass
-  include Contracts;
+  include Contracts
   
   before_and_after_each_call(proc{ puts 'Before from parent' },  proc{ puts 'After from parent' })
   
@@ -79,6 +132,8 @@ end
 class TestClass < ParentTestClass
   include Contracts
   include TestModule
+  
+  includeContracts #Esto es opcional por si se quieren incluir los contratos presentes en módulos (sigo investigando como hacerlo automáticamente)
   
   before_and_after_each_call(proc{ puts 'Before 1' },  proc{ puts 'After 1' })
   before_and_after_each_call(proc{ puts 'Before 2' },  proc{ puts 'After 2' })
---
+++ ruby/lib/invariants.rb
@@ -0,0 +1,49 @@
+require_relative  'contracts.rb'
+
+#El modulo Invariants va a agregar metodos de clase nuevos a la clase (o módulo) que lo incluya
+module Invariants
+  include Contracts
+  
+  #El módulo ClassMethods incluye en si los métodos de clase que van a agregarse a la clase objetivo
+  module InvariantsClassMethods
+    
+    #Este método va a crear un contrato, a partir de un bloque recibido
+    #Dicho contrato va a constar de un proc after que va a ejecutar el bloque recibido en el contexto del objeto 
+    #instanciado a partir de la clase que implementa la invarianza (me expliqué para el tujes no?)
+    def invariant(&block)
+      myBlock = block
+      #Creo un contrato nuevo, solo me interesa el block after
+      before_and_after_each_call(proc{}, proc{|myInstance| 
+        #Ejecuto el bloque en el contexto/scope del objeto que me llega por parametro
+        #Evaluo el resultado como booleano
+        #Si da false tiro una excepción
+        if(!myInstance.instance_eval &myBlock)
+          raise 'Invariant Error'
+        end
+      })
+    end
+  end
+  
+  #Cuando el módulo es incluido
+  def self.included(base)
+    base.extend(InvariantsClassMethods)
+  end
+end
+
+#Clase de ejemplo para testear, deberia fallar al llamar al método "aMethod"
+class TestClass2
+  include Contracts
+  include Invariants
+  
+  invariant {
+    @life > 0
+  }
+  
+  def initialize()
+    @life = 2
+  end
+  
+  def aMethod
+    @life = -1
+  end
+end
