aecc378f4da582a67c045635a66ebf01ae9ea8e5
CesarMateos <cmateos@est.frba.utn.edu.ar>
2019-06-12 14:16:43 -0300

    Archivos Originales del TP SCALA

---
+++ scala/src/main/scala/PlayAudio.scala
@@ -0,0 +1,5 @@
+object PlayAudio extends App {
+  val sariaSong = "F A B B F A B B F A B E D D B C B G E E D D E G E F A B B F A B B F A B E D D B C E B G G D E G E"
+
+  AudioPlayer.reproducir(sariaSong)
+}
