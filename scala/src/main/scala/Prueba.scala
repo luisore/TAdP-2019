05ad20762989b0f529784685ed2d4ff14e8496da
Unknown <trictor98@gmail.com>
2019-06-13 12:06:06 -0300

    primera version con errores

--- scala/src/main/scala/Prueba.scala
+++ scala/src/main/scala/Prueba.scala
@@ -1,5 +1,35 @@
-case object Prueba {
+class Parser(xc: String) {
+  var x: String = xc
+  def sacarRepetido = {
+    var lista: String = ""
+    for(nota <- x){
+      if(nota == '(')
+       for(i <- (x.indexOf('(') +1) to (x.indexOf(')')-1)){
+         lista += x(i)
+       }
+         
+    }
+    lista
+  }
+  def cantidadARepetir = x.find(nota => (nota == '1') || (nota == '2')).get.toInt
+  
+  def insertar = {
+    var listaAInsertar: String =""
+    var repeticiones: Int = this.cantidadARepetir
+    for (i <- 1 to repeticiones){
+      listaAInsertar += this.sacarRepetido
+    }
+    listaAInsertar
+  }
+}
 
-  def materia: String = "tadp"
-
+ 
+object Demo{
+  def main(args: Array[String]){
+    val point = new Parser("ASD2X(ABC)")
+    var listaRespuesta: String = point.sacarRepetido
+    print(point.cantidadARepetir)
+ 
+    
+  }
 }
